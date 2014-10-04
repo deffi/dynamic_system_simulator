75639ec9db2f5aa2130a2352e3173a522463729e
Martin Herrmann <martin@famherrmann.de>
2014-10-04 14:02:33 +0200

    Add axes

--- simple_plot.py
+++ simple_plot.py
@@ -33,12 +33,31 @@ class SimplePlot:
 
         x_range = minmax([series.x_range() for series in self._series])
         y_range = minmax([series.y_range() for series in self._series])
+
+        def transform(p):
+            x = p[0]
+            y = p[1]
+            tx = (w-1)*(x-x_range.min)/(x_range.max-x_range.min)
+            ty = (h-1)*(y-y_range.min)/(y_range.max-y_range.min)
+            return (tx, ty)
+        
+        def paint(p, character, ignore_outside = False):
+            x = round(p[0])
+            y = round(p[1])
+            if ignore_outside and (x<0 or y<0 or x>w-1 or y>h-1):
+                return
+            area[y][x] = character
+        
+        xo, yo = transform((0, 0))
+        for x in range(w):
+            paint((x, yo), "-", ignore_outside = True)
+        for y in range(h):
+            paint((xo, y), "|", ignore_outside = True)
+        paint((xo, yo), "+", ignore_outside = True)
         
         for series in self._series:
-            for xx, yy in zip(series._x, series._y):
-                ix = (w-1)*(xx-x_range.min)/(x_range.max-x_range.min)
-                iy = (h-1)*(yy-y_range.min)/(y_range.max-y_range.min)
-                area[round(iy)][round(ix)] = series._character
+            for x, y in zip(series._x, series._y):
+                paint(transform((x, y)), series._character)
 
         return area
 
@@ -56,11 +75,11 @@ def plot(x, y, w, h):
     p.dump()
 
 if __name__ == "__main__":
-    x = [0.1*x for x in range(100)]
-    y1 = [math.sin(x) for x in x]
+    x = [0.1*x-1 for x in range(100)]
+    y1 = [math.sin(x)+0.5 for x in x]
     y2 = [math.cos(x) for x in x]
 
     p=SimplePlot()
     p.plot(x, y1)
     p.plot(x[30:50], y2[30:50], character="*")
-    p.dump(w=80, h=15)
+    p.dump(w=80, h=15, background = " ")
