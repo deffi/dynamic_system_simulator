f63dd1d54b758feb06fbdec70295bcb5614d995e
Martin Herrmann <martin@famherrmann.de>
2014-10-09 22:29:45 +0200

    Plot two variables

--- dss.py
+++ dss.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -7,8 +9,6 @@ from systems import Pendulum, TimeFunction
 
 #from simple_plot.simple_plot import plot
 
-# t = np.arange(0, 10, 0.1)
-# x = np.zeros(np.size(t))
 
 step = lambda t: 1 if t>0 else 0
 rect = lambda t: 1 if t>0 and t<1 else 0
@@ -16,19 +16,26 @@ rect = lambda t: 1 if t>0 and t<1 else 0
 
 s = System("system")
 pendulum = s.add_subsystem(Pendulum("pendulum", mass=0.5, stiffness=5*1.5, damping=0.1))
-gravity = s.add_subsystem(TimeFunction("gravity", lambda t: -5*rect((t-5)/5)))
+gravity = s.add_subsystem(TimeFunction("gravity", lambda t: -1*rect((t-5)/5)))
 
 pendulum.mass.position = 1
 pendulum.variables.gravity.connect(gravity.variables.value)
 
 print_system(s)
 
+time = 16
+step = 0.05
 
-t = [t*0.1 for t in range(120)]
-x = [0] * len(t)
+t = np.arange(0, time, step)
+x = np.zeros(np.size(t))
+y = np.zeros(np.size(t))
+
+# t = [t*step for t in range(math.floor(time/step))]
+# x = [0] * len(t)
  
 for i in range(len(t)):
     x[i] = pendulum.mass.position
+    y[i] = gravity.value
  
     dt = t[i]-t[i-1] if i>0 else None
     if i>0:
@@ -37,5 +44,5 @@ for i in range(len(t)):
 #plot(t, x, w=120, h=15, background = " ")
 
 
-plt.plot(t, x)        
+plt.plot(t, x, t, y)        
 plt.show()
