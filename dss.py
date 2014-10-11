b7f3b3133dfe748b3abc9ef8bb3ec0e6687f01c9
Martin Herrmann <martin@famherrmann.de>
2014-10-11 22:13:07 +0200

    Add SystemRunner, clean up

--- dss.py
+++ dss.py
@@ -1,49 +1,30 @@
-#import math
-
-import numpy as np
 from matplotlib import pyplot as plt
+#from simple_plot.simple_plot import plot as splot
 
+import functions as fn
 from system import System
 from system import print_system
 from systems import Pendulum, TimeFunction
-
-#from simple_plot.simple_plot import plot as splot
-
-step = lambda t: 1 if t>0 else 0
-rect = lambda t: 1 if t>0 and t<1 else 0
+from system_runner import SystemRunner
 
 s = System("system")
 pendulum = s.add_subsystem(Pendulum("pendulum", mass=0.5, stiffness=5*1.5, damping=0.1))
-gravity = s.add_subsystem(TimeFunction("gravity", lambda t: -1*rect((t-5)/5)))
+gravity = s.add_subsystem(TimeFunction("gravity", lambda t: -0.20*fn.rect((t-5)/5)))
 
-#pendulum.mass.position.set(0.1)
-pendulum.mass.variables_wrapper.position=0.1
+pendulum.mass.position.set(0.1)
 pendulum.gravity.connect(gravity.value)
 
 print_system(s)
 
-time = 16
-step = 0.05
+runner = SystemRunner(s)
+runner.add_variable(pendulum.mass.position)
+runner.add_variable(gravity.value)
+runner.run(16, 0.05)
 
-t = np.arange(0, time, step)
-x = np.zeros(np.size(t))
-y = np.zeros(np.size(t))
+pos = runner.variables[pendulum.mass.position]
+grav = runner.variables[gravity.value]
+t = runner.t
 
-# t = [t*step for t in range(math.floor(time/step))]
-# x = [0] * len(t)
- 
- 
-for i in range(len(t)):
-    #print(pendulum.spring.displacement.get(), pendulum.spring.force.get())
-    x[i] = float(pendulum.mass.position)
-    y[i] = float(gravity.value)
- 
-    dt = t[i]-t[i-1] if i>0 else None
-    if i>0:
-        s.update(s.variables_wrapper, t[i], dt)
-
+plt.plot(t, pos, t, grav)        
 #splot(t, x, w=120, h=15, background = " ")
-
-
-plt.plot(t, x, t, y)        
 plt.show()
---
+++ functions.py
@@ -0,0 +1,3 @@
+step = lambda t: 1 if t>0 else 0
+rect = lambda t: 1 if t>0 and t<1 else 0
+
